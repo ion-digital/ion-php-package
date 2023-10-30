e74a0f82b8e31f75ca19271a88a02e1adf686a6c
jmeyer-vlsystems <92323513+jmeyer-vlsystems@users.noreply.github.com>
2023-10-30 23:54:23 +0200

    WIP

--- index.php
+++ index.php
@@ -4,56 +4,90 @@
  * See license information at the package root in LICENSE.md
  */
 
-// header(filter_input(INPUT_SERVER, 'PROTOCOL', FILTER_DEFAULT) . " 404 Not Found");
-// exit;
+(new class {
 
-// if ((PHP_MAJOR_VERSION === 5 && PHP_MINOR_VERSION < 6) || (PHP_MAJOR_VERSION < 5)) {
-    
-//     die('This version of Versioning is currently running on PHP version <em>' . PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION . '</em> - PHP version 5.6 or higher is required.');
-// }
+    private const VENDOR = "ion";
+    private const PROJECT = "packaging";
+    private const CLASSNAME = "\ion\Package";
+    private const BUILDS_DIR = "builds";
+    private const SOURCE_DIR = "source";
+    private const SOURCE_CLASSES_DIR = "classes";
+    private const SOURCE_TRAITS_DIR = "traits";
+    private const SOURCE_INTERFACES_DIR = "interfaces";
+    private const FORWARD_SLASH = "/";
+    private const BACKWARD_SLASH = "\\";
+    private const PHP_VERSION_SEPARATOR = ".";
+    private const PHP_FILE_EXTENSION = ".php";
 
-// $autoLoadPath = realpath(__DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+    public static function create(): object {
 
-// if (file_exists($autoLoadPath)) {
-    
-//     require_once($autoLoadPath);
-// }
+        try {
 
-if(!class_exists("\ion\Package")) {
-
-    spl_autoload_register(function(string $className) {
-
-        $dirs = [
-            
-            'source/classes/',
-            'source/traits/',
-            'source/interfaces/',
-            'builds/' . PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION,
-            'builds/' . PHP_MAJOR_VERSION,
-        ];
-
-        foreach($dirs as $dir) {
-        
-            $classPath = __DIR__ 
-            . DIRECTORY_SEPARATOR 
-            . str_replace("/", DIRECTORY_SEPARATOR, $dir) 
-            . DIRECTORY_SEPARATOR 
-            . str_replace("\\", DIRECTORY_SEPARATOR, $className) 
-            . '.php';
-
-            $classPath = realPath($classPath);
-
-            if (file_exists($classPath)) {
+            return (self::CLASSNAME)::create(
                 
-                require_once($classPath);
-                break;
-            }
+                self::VENDOR, 
+                self::PROJECT, 
+                true, 
+            
+                function() {
+            
+                    self::load();
+                }, 
+
+                __FILE__
+            );
         }
+        catch(\Throwable $throwable) {
+
+            if(!self::load()) {
+
+                throw $throwable;
+            }
+
+            return self::create();
+        }
+    }
+
+    private static function load(): bool {
+
+        if(class_exists(self::CLASSNAME)) {
+
+            return false;
+        }
+
+        spl_autoload_register(function(string $className) {
         
-    }, true, true);
-}
+            $dirs = [
+                
+                self::SOURCE_DIR . DIRECTORY_SEPARATOR . self::SOURCE_CLASSES_DIR . DIRECTORY_SEPARATOR,
+                self::SOURCE_DIR . DIRECTORY_SEPARATOR . self::SOURCE_TRAITS_DIR . DIRECTORY_SEPARATOR,
+                self::SOURCE_DIR . DIRECTORY_SEPARATOR . self::SOURCE_INTERFACES_DIR . DIRECTORY_SEPARATOR,
+                self::BUILDS_DIR . DIRECTORY_SEPARATOR . PHP_MAJOR_VERSION . self::PHP_VERSION_SEPARATOR . PHP_MINOR_VERSION . DIRECTORY_SEPARATOR,
+                self::BUILDS_DIR . DIRECTORY_SEPARATOR . PHP_MAJOR_VERSION . DIRECTORY_SEPARATOR,
+            ];
+    
+            foreach($dirs as $dir) {
+            
+                $classPath = __DIR__ 
 
-\ion\Package::create("ion", "packaging", true, function() {
-
-    return;
-});
+                    . DIRECTORY_SEPARATOR 
+                    . str_replace(self::FORWARD_SLASH, DIRECTORY_SEPARATOR, $dir) 
+                    . DIRECTORY_SEPARATOR 
+                    . str_replace(self::BACKWARD_SLASH, DIRECTORY_SEPARATOR, $className) 
+                    . self::PHP_FILE_EXTENSION;
+    
+                $classPath = realPath($classPath);
+    
+                if (file_exists($classPath)) {
+                    
+                    require_once($classPath);
+                    break;
+                }
+            }
+            
+        }, true, false);  
+        
+        return true;
+    }
+    
+})::create();
