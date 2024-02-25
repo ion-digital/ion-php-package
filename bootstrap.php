1fc938876fa8e51eb69f9610680d53a8dea29c59
jmeyer-vlsystems <92323513+jmeyer-vlsystems@users.noreply.github.com>
2024-02-25 23:16:34 +0200

    Updated bootstrap error handling

--- bootstrap.php
+++ bootstrap.php
@@ -19,27 +19,76 @@
 
     public static function create(string $entryFile): void {
 
-        if(defined(self::LOAD_DEFINITION))
-            return;
+        try {
 
-        $baseDir = dirname($entryFile);
+            if(defined(self::LOAD_DEFINITION))
+                return;
 
-        require_once(realpath( $baseDir . DIRECTORY_SEPARATOR . self::PACKAGING_INCLUDE_FILENAME ));
+            $baseDir = dirname($entryFile);
 
-        define(self::LOAD_DEFINITION, $entryFile);
+            require_once(realpath( $baseDir . DIRECTORY_SEPARATOR . self::PACKAGING_INCLUDE_FILENAME ));
 
-		$composerPath = null;
-		
-		foreach(self::COMPOSER_AUTOLOAD_PATHS as $autoloaderPath) {
-			
-            $composerPath = realpath( $baseDir . DIRECTORY_SEPARATOR . $autoloaderPath . DIRECTORY_SEPARATOR . self::COMPOSER_AUTOLOAD_FILENAME );
+            define(self::LOAD_DEFINITION, $entryFile);
 
-			if(!empty($composerPath))
-                break;            
-		}
+            $composerPath = null;
+            
+            foreach(self::COMPOSER_AUTOLOAD_PATHS as $autoloaderPath) {
+                
+                $composerPath = realpath( $baseDir . DIRECTORY_SEPARATOR . $autoloaderPath . DIRECTORY_SEPARATOR . self::COMPOSER_AUTOLOAD_FILENAME );
 
-        if(!empty($composerPath))
-            require_once($composerPath);
+                if(!empty($composerPath))
+                    break;            
+            }
+
+            if(!empty($composerPath))
+                require_once($composerPath);
+        }
+        catch(\Exception $exception) {
+
+            self::handleException($exception);
+            exit;
+        }        
+        catch(\Throwable $throwable) {
+
+            self::handleThrowable($throwable);
+            exit;
+        }
+    }
+
+    private static function handleException(\Exception $exception): void {
+
+        self::handleError($exception, Exception::class);
+    }    
+
+    private static function handleThrowable(\Throwable $throwable): void {
+
+        self::handleError($throwable, Throwable::class);
+    }
+
+    private static function handleError(\Throwable $throwable, string $string): void {
+
+        ?>
+<html>
+    <head>
+        <title>Bootstrapping Error</title>
+    </head>
+    <body>        
+        <h1>Bootstrapping Error Occurred</h1>
+
+        <div><em>(caught in <?php echo __FILE__; ?>)</em></div>
+
+        <h2>Type</h2>
+        <div><?php echo $string; ?></div>
+
+        <h2>Message</h2>
+        <div><?php echo $throwable->getMessage(); ?>.</div>
+
+        <h2>Source</h2>
+        <div><strong><?php echo $throwable->getFile(); ?></strong> on line <strong><?php echo $throwable->getLine(); ?></strong></div>                            
+    </body>
+</html>
+        <?php
+
     }
 
 })::create(__FILE__);
